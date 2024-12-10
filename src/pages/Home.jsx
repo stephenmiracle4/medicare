331fd4af651860912c4b1954590e1b4b29947591
eunice-ogboye <ogboyeeunice@gmail.com>
2024-12-10 23:12:56 +0100

    init project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from '../components/Nav'
+
+function Home() {
+  return (
+    <div>
+        <Nav/>
+        
+       <p className='text-red-600 text-3xl'> Home page</p>
+        
+        </div>
+  )
+}
+
+export default Home
