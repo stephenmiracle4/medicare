046314dc193e0f592fb523ce15da66cab1ee008e
eunice-ogboye <ogboyeeunice@gmail.com>
2024-12-11 21:35:01 +0100

    app router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+// const About = () => <div>About Page</div>;
 
-function App() {
-  return <div>
-    
-    <Home />
-  </div>
-}
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
