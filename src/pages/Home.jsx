39810d08c407b890831738d2933ddf4dedc37915
stephen miracle <109660118+stephenmiracle4@users.noreply.github.com>
2024-12-12 00:18:18 +0100

    commit changes
    commit changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Nav'
 import HeroSection from '../components/HeroSection'
+import ServiceSection from './ServiceSection'
 
 function Home() {
   return (
     <div>
         <Navbar/>
         <HeroSection />
+        <ServiceSection/>
     </div>
   )
 }
---
+++ src/pages/ServiceSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// import images
+
+
+function ServiceSection() {
+  return (
+    <div>ServiceSection</div>
+  )
+}
+
+export default ServiceSection
