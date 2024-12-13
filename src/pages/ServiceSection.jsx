4b02c858fde96af1f2e7ae31fccdfcb5c385a90e
stephen miracle <109660118+stephenmiracle4@users.noreply.github.com>
2024-12-13 02:02:55 +0100

    commit changes
    commit changes

--- src/pages/ServiceSection.jsx
+++ src/pages/ServiceSection.jsx
@@ -6,96 +6,90 @@ function ServiceSection() {
   return (
     <div>ServiceSection
 
-<div>
-    <img src="/images/services/background3.png" alt="background" />
+        <img src="/images/services/background3.png" alt="background" class="w-100% h-160 object-cover shadow-lg rounded-lg bg-no-repeat"/>
 
-    <div id="cssmenu" class="animated">
-        <div class="uni-icons-close"><i class="fa fa-times" aria-hidden="true"></i></div>
-        <ul class="nav navbar-nav animated">
-            <li class="has-sub"><a href="#">Home</a>
-                <ul>
-                    <li><a href="index-2.html">Home 1</a></li>
-                    <li><a href="01_02_home_2.html">Home 2</a></li>
-                    <li><a href="01_03_home_3.html">Home 3</a></li>
-                </ul>
-            </li>
-            <li class="has-sub"><a href='#'>Page</a>
-                <ul>
-                    <li><a href="02_01_about.html">About</a></li>
-                    <li class="has-sub"><a href="#">doctor</a>
-                        <ul>
-                            <li><a href="02_02_doctor.html">Doctors </a></li>
-                            <li><a href="02_03_doctor_details.html">Doctors Details</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="02_04_contact.html">Contact</a></li>
-                    <li><a href="02_05_gallery.html">Gallery</a></li>
-                </ul>
-            </li>
-            <li><a href="03_01_services.html">Services</a></li>
-            <li class="has-sub"><a href="#">Departments</a>
-                <ul>
-                    <li><a href="04_01_departments.html">Departments</a></li>
-                    <li><a href="04_02_single_department.html">Single Departments</a></li>
-                </ul>
-            </li>
-            <li class="has-sub"><a href='#'>Blog</a>
-                <ul>
-                    <li><a href="05_01_blog_list.html">Blog list</a></li>
-                    <li><a href="05_02_blog_grid.html">Blog Grid</a></li>
-                    <li><a href="05_03_single_post.html">Single post</a></li>
-                </ul>
-            </li>
-            <li class="has-sub"><a href='#'>shop</a>
-                <ul>
-                    <li><a href="06_01_shop.html">Shop</a></li>
-                    <li><a href="06_02_single_product.html">Single Product</a></li>
-                    <li><a href="06_03_cart.html">Cart</a></li>
-                    <li><a href="06_04_checkout.html">Checkout</a></li>
-                </ul>
-            </li>
-            <li class="has-sub"><a href='#'>Element</a>
-                <ul>
-                    <li class="has-sub"><a href="#">Short Code 1</a>
-                        <ul>
-                            <li><a href="07_01_buttons.html"><i class="fa fa-plus-square" aria-hidden="true"></i>Buttons</a></li>
-                            <li><a href="07_02_icons_box.html"><i class="fa fa-cube" aria-hidden="true"></i>Icon Box</a></li>
-                            <li><a href="07_03_progress.html"><i class="fa fa-tasks" aria-hidden="true"></i>Process Bar</a></li>
-                            <li><a href="07_04_tabs.html"><i class="fa fa-columns" aria-hidden="true"></i>Tabs</a></li>
-                        </ul>
-                    </li>
-                    <li class="has-sub"><a href="#">Short Code 2</a>
-                        <ul>
-                            <li><a href="07_05_accordion.html"><i class="fa fa-list" aria-hidden="true"></i>Accordion</a></li>
-                            <li><a href="07_06_counter.html"><i class="fa fa-tachometer" aria-hidden="true"></i>Counter</a></li>
-                            <li><a href="07_07_testimonials.html"><i class="fa fa-comments-o" aria-hidden="true"></i>Testimonials</a></li>
-                            <li><a href="07_08_typography.html"><i class="fa fa-text-width" aria-hidden="true"></i>Typography</a></li>
-                        </ul>
-                    </li>
-                    <li class="has-sub"><a href="#">Short Code 3</a>
-                        <ul>
-                            <li><a href="07_09_partner.html"><i class="fa fa-handshake-o" aria-hidden="true"></i> Partner</a></li>
-                            <li><a href="07_10_team.html"><i class="fa fa-users" aria-hidden="true"></i> Team</a></li>
-                            <li><a href="07_11_item_list.html"><i class="fa fa-list-ol" aria-hidden="true"></i> Item List</a></li>
-                            <li><a href="07_12_divider.html"><i class="fa fa-chain-broken" aria-hidden="true"></i>Dividers</a></li>
-                        </ul>
-                    </li>
-                    <li class="has-sub"><a href="#">Short Code 4</a>
-                        <ul>
-                            <li><a href="07_13_columns.html"><i class="fa fa-bar-chart" aria-hidden="true"></i> Columns</a></li>
-                            <li><a href="07_14_pricing_table.html"><i class="fa fa-address-card-o" aria-hidden="true"></i> Pricing table</a></li>
-                            <li><a href="#"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> 404 Pages</a></li>
-                            <li><a href="#"><i class="fa fa-repeat" aria-hidden="true"></i> Comming soon</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-            <li><a href="02_04_contact.html">Contact</a></li>
-        </ul>
-        <div class="clearfix"></div>
+
+<div id="cssmenu" class="animated">
+    <div class="uni-icons-close cursor-pointer text-xl p-4">
+        <i class="fa fa-times" aria-hidden="true"></i>
     </div>
+    <ul class="nav navbar-nav space-y-4">
+        <li class="relative group">
+            <a href="#" class="block py-2 px-4 text-lg text-gray-800 hover:bg-gray-200">Home</a>
+            <ul class="absolute left-0 hidden group-hover:block bg-white shadow-md mt-2 space-y-2">
+                <li><a href="index-2.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200">Home 1</a></li>
+                <li><a href="01_02_home_2.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200">Home 2</a></li>
+                <li><a href="01_03_home_3.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200">Home 3</a></li>
+            </ul>
+        </li>
+        <li class="relative group">
+            <a href="#" class="block py-2 px-4 text-lg text-gray-800 hover:bg-gray-200">Page</a>
+            <ul class="absolute left-0 hidden group-hover:block bg-white shadow-md mt-2 space-y-2">
+                <li><a href="02_01_about.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200">About</a></li>
+                <li class="relative group">
+                    <a href="#" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200">Doctor</a>
+                    <ul class="absolute left-0 hidden group-hover:block bg-white shadow-md mt-2 space-y-2">
+                        <li><a href="02_02_doctor.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200">Doctors</a></li>
+                        <li><a href="02_03_doctor_details.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200">Doctor Details</a></li>
+                    </ul>
+                </li>
+                <li><a href="02_04_contact.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200">Contact</a></li>
+                <li><a href="02_05_gallery.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200">Gallery</a></li>
+            </ul>
+        </li>
+        <li><a href="03_01_services.html" class="block py-2 px-4 text-lg text-gray-800 hover:bg-gray-200">Services</a></li>
+        <li class="relative group">
+            <a href="#" class="block py-2 px-4 text-lg text-gray-800 hover:bg-gray-200">Departments</a>
+            <ul class="absolute left-0 hidden group-hover:block bg-white shadow-md mt-2 space-y-2">
+                <li><a href="04_01_departments.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200">Departments</a></li>
+                <li><a href="04_02_single_department.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200">Single Department</a></li>
+            </ul>
+        </li>
+        <li class="relative group">
+            <a href="#" class="block py-2 px-4 text-lg text-gray-800 hover:bg-gray-200">Blog</a>
+            <ul class="absolute left-0 hidden group-hover:block bg-white shadow-md mt-2 space-y-2">
+                <li><a href="05_01_blog_list.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200">Blog List</a></li>
+                <li><a href="05_02_blog_grid.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200">Blog Grid</a></li>
+                <li><a href="05_03_single_post.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200">Single Post</a></li>
+            </ul>
+        </li>
+        <li class="relative group">
+            <a href="#" class="block py-2 px-4 text-lg text-gray-800 hover:bg-gray-200">Shop</a>
+            <ul class="absolute left-0 hidden group-hover:block bg-white shadow-md mt-2 space-y-2">
+                <li><a href="06_01_shop.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200">Shop</a></li>
+                <li><a href="06_02_single_product.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200">Single Product</a></li>
+                <li><a href="06_03_cart.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200">Cart</a></li>
+                <li><a href="06_04_checkout.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200">Checkout</a></li>
+            </ul>
+        </li>
+        <li class="relative group">
+            <a href="#" class="block py-2 px-4 text-lg text-gray-800 hover:bg-gray-200">Element</a>
+            <ul class="absolute left-0 hidden group-hover:block bg-white shadow-md mt-2 space-y-2">
+                <li class="relative group">
+                    <a href="#" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200">Short Code 1</a>
+                    <ul class="absolute left-0 hidden group-hover:block bg-white shadow-md mt-2 space-y-2">
+                        <li><a href="07_01_buttons.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200"><i class="fa fa-plus-square" aria-hidden="true"></i> Buttons</a></li>
+                        <li><a href="07_02_icons_box.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200"><i class="fa fa-cube" aria-hidden="true"></i> Icon Box</a></li>
+                        <li><a href="07_03_progress.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200"><i class="fa fa-tasks" aria-hidden="true"></i> Process Bar</a></li>
+                        <li><a href="07_04_tabs.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200"><i class="fa fa-columns" aria-hidden="true"></i> Tabs</a></li>
+                    </ul>
+                </li>
+                <li class="relative group">
+                    <a href="#" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200">Short Code 2</a>
+                    <ul class="absolute left-0 hidden group-hover:block bg-white shadow-md mt-2 space-y-2">
+                        <li><a href="07_05_accordion.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200"><i class="fa fa-list" aria-hidden="true"></i> Accordion</a></li>
+                        <li><a href="07_06_counter.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200"><i class="fa fa-tachometer" aria-hidden="true"></i> Counter</a></li>
+                        <li><a href="07_07_testimonials.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200"><i class="fa fa-comments-o" aria-hidden="true"></i> Testimonials</a></li>
+                        <li><a href="07_08_typography.html" class="block py-2 px-4 text-sm text-gray-600 hover:bg-gray-200"><i class="fa fa-text-width" aria-hidden="true"></i> Typography</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li><a href="02_04_contact.html" class="block py-2 px-4 text-lg text-gray-800 hover:bg-gray-200">Contact</a></li>
+    </ul>
+    <div class="clearfix"></div>
+</div>
 
-  </div>
 
     </div>
   )
